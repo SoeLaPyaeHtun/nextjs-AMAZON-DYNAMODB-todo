54f409fcefb9065b0152d22a9170d0ffcee1de71
SoeLaPyaeHtun <soelapyaehtun@gmail.com>
2023-09-06 16:25:18 +0800

    added query to listing todo

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,39 @@
-import Image from 'next/image'
+"use client";
+import { useEffect, useState } from 'react';
+import { listTodos } from '../src/graphql/queries';
+import { API } from 'aws-amplify';
+import { Amplify } from "aws-amplify";
+import config from "../src/aws-exports";
+
+Amplify.configure(config)
+
 
 export default function Home() {
+
+  const [ todos , setTodo ] = useState([])
+  useEffect(() => {
+    fetchData();
+    
+  },[])
+
+  const fetchData = async () => {
+    const todoDatas = await API.graphql({
+      query : listTodos
+    })
+    setTodo(todoDatas.data.listTodos.items)
+  }
+
+ 
+
   return (
-    <main>
-      <h1>hello from page</h1>
-    </main>
+   <div>
+    <h1>Todo List</h1>
+    {
+      todos.map((todo, index) => (
+        <p key={index}>{todo.name}</p>
+      ))
+    }
+   </div>
   )
 }
+
